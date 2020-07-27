da780062e7837fe54af853bf4808f2cc5c25260a
tao <xdd666@foxmail.com>
2020-07-27 17:01:52 +0800

    调整api

--- lib/http/api.dart
+++ lib/http/api.dart
@@ -3,21 +3,21 @@ class ApiUrl{
 
 
   //获取轮播信息 首页Bannner
-  static String GET_BANNER_URL = "https://www.wanandroid.com/banner/json";
+  static const String GET_BANNER_URL = "https://www.wanandroid.com/banner/json";
 
   //获取轮播信息 首页Bannner
-  static String GET_HOME_ARTICLE = "https://www.wanandroid.com/article/list/";
+  static const String GET_HOME_ARTICLE = "https://www.wanandroid.com/article/list/";
 
   // 获取体系数据
-  static String GET_TREE = "https://www.wanandroid.com/tree/json";
+  static const String GET_TREE = "https://www.wanandroid.com/tree/json";
   //体系文章详情
-  static String GET_TREE_DETAIL = "https://www.wanandroid.com/article/list/0/json";
+  static const String GET_TREE_DETAIL = "https://www.wanandroid.com/article/list/0/json";
 
   //获取导航数据
-  static String GET_NAVI_INFO = "https://www.wanandroid.com/navi/json";
+  static const String GET_NAVI_INFO = "https://www.wanandroid.com/navi/json";
 
   //获取项目数据
-  static String GET_PROJECT_INFO = "https://www.wanandroid.com/project/tree/json";
-  static String GET_PROJECT_DETAIL = "https://www.wanandroid.com/project/list/1/json";
+  static const String GET_PROJECT_INFO = "https://www.wanandroid.com/project/tree/json";
+  static const String GET_PROJECT_DETAIL = "https://www.wanandroid.com/project/list/1/json";
 
 }
